9d001238ca4a7472850c20c1418b3700aa187764
Andrea Bombarda <a.bombarda@studenti.unibg.it>
2022-03-28 09:27:39 +0200

    Update ct_comp_evaluation.py

--- ToolEvaluator/ct_comp_evaluation.py
+++ ToolEvaluator/ct_comp_evaluation.py
@@ -2,6 +2,8 @@ import argparse
 from os import listdir
 from os.path import isfile, join
 import csv
+import pandas as pd 
+import math
 
 # ====================================================================================================
 # Returns the time of the corresponding execution
@@ -17,10 +19,10 @@ def get_time(file, file_path):
                 time_str = row[len("Elapsed (wall clock) time (h:mm:ss or m:ss):"):].strip()
                 # Only minutes and seconds
                 if (time_str.count(":") == 1):
-                    time_seconds = time_seconds + int(time_str.split(":")[0]) * 60 + float(time_str.split(":")[1])
+                    time_seconds = round(time_seconds + int(time_str.split(":")[0]) * 60 + float(time_str.split(":")[1]), 2)
                 else:
-                    time_seconds = time_seconds + int(time_str.split(":")[0]) * 3600 + int(time_str.split(":")[1]) * 60 + \
-                        float(time_str.split(":")[2])
+                    time_seconds = round(time_seconds + int(time_str.split(":")[0]) * 3600 + int(time_str.split(":")[1]) * 60 + \
+                        float(time_str.split(":")[2]), 2)
                 break
 
         f.close
@@ -78,6 +80,34 @@ def main(file_path, output_file):
     out_file.close
 # ====================================================================================================
 
+# ====================================================================================================
+# Extracts the best results for each tool over the three iterations
+def extract_best_results(output_file):
+    df_output = pd.read_csv(output_file + ".csv", delimiter=",")
+    df_aggregate = pd.DataFrame(columns=["ToolName","ModelName","Strength","TimeSeconds","Size"])
+    model_names = df_output["ModelName"].unique()
+    generator_names = df_output["ToolName"].unique()
+    strengths = df_output["Strength"].unique()
+
+    for strength in strengths:
+        for modelName in model_names:
+            for generatorName in generator_names:
+                filtered_df_output = df_output[(df_output.ToolName.eq(generatorName)) & (df_output.ModelName.eq(modelName)) & \
+                    (df_output.Strength.eq(strength))]
+                min_time = filtered_df_output.TimeSeconds.min() if not math.isnan(filtered_df_output.TimeSeconds.min()) else -1
+                min_size = filtered_df_output.Size.min() if not math.isnan(filtered_df_output.Size.min()) else -1
+                df_aggregate = df_aggregate.append({
+                    "ToolName": generatorName,
+                    "ModelName": modelName,
+                    "Strength": strength,
+                    "TimeSeconds": min_time,
+                    "Size": min_size
+                }, ignore_index=True)
+
+    df_aggregate.to_csv(output_file + "_best.csv", index = False)
+# ====================================================================================================
+
+# ====================================================================================================
 # The program should be called by passing two arguments:
 # - the path in which the test suite files and results are stored
 # - the name of the output file, in which the aggregated results are stored
@@ -91,3 +121,4 @@ if __name__ == "__main__":
     main(args.r, args.o)
 
     # Extract the best results
+    extract_best_results(args.o)
