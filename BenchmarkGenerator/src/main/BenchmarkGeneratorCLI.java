82d8454e8e73bd98574f641a708d3ce01028017b
Andrea Bombarda <a.bombarda@studenti.unibg.it>
2023-10-12 11:08:13 +0200

    Update BenchmarkGeneratorCLI.java

--- BenchmarkGenerator/src/main/BenchmarkGeneratorCLI.java
+++ BenchmarkGenerator/src/main/BenchmarkGeneratorCLI.java
@@ -4,16 +4,13 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.io.InputStream;
 import java.lang.reflect.Type;
 import java.util.ArrayList;
-import java.util.List;
 import java.util.concurrent.Callable;
 
 import org.apache.log4j.Level;
 import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
-import org.eclipse.xtend.lib.macro.declaration.TypeReference;
 import org.sosy_lab.common.configuration.InvalidConfigurationException;
 import org.sosy_lab.java_smt.api.SolverException;
 
