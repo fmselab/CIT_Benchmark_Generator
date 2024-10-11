319ca1b73729953a780a37e7e65fdd11c415547c
Andrea Bombarda <a.bombarda@studenti.unibg.it>
2024-10-11 09:04:20 +0200

    Add some new mutation

---
+++ BenchmarkGenerator/src/util/genetics/mutations/ConstraintAndToOrMutation.java
@@ -0,0 +1,50 @@
+package util.genetics.mutations;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+import generators.Track;
+import models.Model;
+
+public class ConstraintAndToOrMutation extends ConstraintOperatorSubstitutionMutation {
+
+
+	/**
+	 * Builds a new ConstraintImpliesToDblImpliesMutation object
+	 * 
+	 * @param p the probability for applying the mutation
+	 */
+	public ConstraintAndToOrMutation(float p) {
+		super(p);
+	}
+
+	@Override
+	public List<Model> apply(List<Model> selectedCandidates, Random rng) {
+
+		List<Model> mutatedPopulation = new ArrayList<Model>(selectedCandidates.size());
+		for (Model m : selectedCandidates) {
+			mutatedPopulation.add(mutateModel(m, rng));
+		}
+		return mutatedPopulation;
+
+	}
+
+	Model mutateModel(Model m, Random rng) {
+		Track track = m.getGeneratorConfiguration().TRACK;
+		// Unconstrained tracks do not support this mutation
+		if (track == Track.MCA || track == Track.UNIFORM_ALL || track == Track.UNIFORM_BOOLEAN) {
+			return m;
+		}
+
+		// Check the probability
+		if (rng.nextFloat(0, 1) < probability)
+			return m;
+
+		// Constrained tracks
+		Model mTemp = changeOperator(m, rng, " AND ", " OR ");
+
+		return mTemp;
+	}
+
+}
---
+++ BenchmarkGenerator/src/util/genetics/mutations/ConstraintOperatorSubstitutionMutation.java
@@ -0,0 +1,69 @@
+package util.genetics.mutations;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.stream.Collectors;
+
+import org.apache.commons.lang3.StringUtils;
+import org.uncommons.watchmaker.framework.EvolutionaryOperator;
+
+import models.Model;
+import models.constraints.Constraint;
+
+public abstract class ConstraintOperatorSubstitutionMutation implements EvolutionaryOperator<Model> {
+
+	/**
+	 * The probability for applying the mutation
+	 */
+	float probability;
+
+	/**
+	 * Builds a new ConstraintOperatorSubstitutionMutation object
+	 * 
+	 * @param p the probability for applying the mutation
+	 */
+	public ConstraintOperatorSubstitutionMutation(float p) {
+		this.probability = p;
+	}
+
+	@Override
+	public List<Model> apply(List<Model> selectedCandidates, Random rng) {
+
+		List<Model> mutatedPopulation = new ArrayList<Model>(selectedCandidates.size());
+		for (Model m : selectedCandidates) {
+			mutatedPopulation.add(mutateModel(m, rng));
+		}
+		return mutatedPopulation;
+
+	}
+
+	abstract Model mutateModel(Model m, Random rng);
+
+	Model changeOperator(Model m, Random rng, String from, String to) {
+		Model mTemp = m;
+		
+		List<Constraint> constraintList = mTemp.getConstraints();
+		constraintList = constraintList.stream().filter(x -> x.toString().contains(from)).collect(Collectors.toList());
+		
+		if (constraintList.size() > 0) {
+			// Choose randomly one of the constraints including the operator
+			int constraintIndexToUpdate = rng.nextInt(0, constraintList.size());
+			// Count how many operators there are
+			int countOperator = StringUtils.countMatches(constraintList.get(constraintIndexToUpdate).toString(), from);
+			// Choose which one to update
+			int operatorIndex = rng.nextInt(0, countOperator);
+			
+			int i = 0;
+			for (Constraint c : m.getConstraints()) {
+				if (c.toString().equals(constraintList.get(constraintIndexToUpdate).toString())) {
+					
+					break;
+				}
+				i++;
+			}
+		}
+		return mTemp;
+	}
+
+}
